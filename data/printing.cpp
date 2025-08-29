2f65dabea0517881f614117a12f12720f2dcb4ec
AkhmetzhanK <akhmetzhan.kussainov@gmail.com>
2025-08-29 11:45:39 +0500

    A minor change to data prefixes; all tree subtypes now have tree_ as prefix.

--- data/printing.cpp
+++ data/printing.cpp
@@ -7,35 +7,59 @@ data::tree::print
 ( std::ostream& out ) const 
 {
 	switch( sel() ) {
-	case prim_empty:
-		return;
+	case tree_empty:
+		break;
 
-	case prim_bool:
+	case tree_bool:
 		if( view_bool(). b() )
 			out << "true" << std::endl;	
 		else
 			out << "false" << std::endl;
-		return;
+		break;
 
-	case prim_char:
+	case tree_char:
 		out << view_char(). c() << std::endl;
-		return;
+		break;
 
-    case prim_sel:
+    case tree_sel:
 		out << view_sel(). s() << std::endl;
-		return;
+		break;
 
-	case prim_u64:
+	case tree_u64:
 		out << view_u64(). i() << std::endl;
-		return;
+		break;
 
-	case prim_int:
+	case tree_int:
 		out << view_int(). i() << std::endl;
-		return;
+		break;
 
-	case prim_double:
+	case tree_double:
 		out << view_double(). d() << std::endl;
-		return;
+		break;
+
+	case tree_tuple:
+	{
+		auto data_tuple = view_nary();
+		
+		out << "( ";
+		for( size_t i = 0; i < data_tuple. size(); ++i ) {
+			if( i ) out << " ,";
+			out << data_tupe. vect(i);
+		}
+		out << " )";
+	} break;
+
+	case tree_array:
+	{
+		auto data_array = view_nary();
+		
+		out << "( ";
+		for( size_t i = 0; i < data_array. size(); ++i ) {
+			if( i ) out << " ,";
+			out << data_array. vect(i);
+		}
+		out << " )";
+	} break;
 
 	default:
 		std::cerr << "data::tree::print() : unrecognized selector : " << sel() << '\n' << std::endl;
